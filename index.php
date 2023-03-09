fbb5ee5fd088cb02e4dda4bcdc4109df017891ed
jipverhoeven <127309178+jipdipdestip@users.noreply.github.com>
2023-03-09 14:16:50 +0100

    benis

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>home</title>
+    <link rel=stylesheet href=index.css>
 </head>
 <body>
-    <p>kaas</p>
-a
-    ja
-    <img src="Cheese_in_the_movie.png">
+    <?php include 'header.php'; ?>
+    <input type="text" div class="zoekbalk">
 </body>
 </html>
