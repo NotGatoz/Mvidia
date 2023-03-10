d045cd15a42988984bc0ce802854c758c04e1e77
Jack Kragten <gatoz11.com@gmail.com>
2023-03-10 11:45:42 +0100

    registratie page af

--- php/registratie.php
+++ php/registratie.php
@@ -5,8 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registratie</title>
+    <link rel=stylesheet href=../css/registratie.css>
+
 </head>
 <body>
+  <div class="container">
+    <h1>registreren</h1>
+    <p>vul alles in</p>
+    <hr>
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="email invullen" name="email" id="email" required>
+    <label for="psw"><b>wachtwoord</b></label>
+    <input type="wachtwoord" placeholder="wachtwoord invullen" name="psw" id="psw" required>
+    <label for="psw-repeat"><b>herhaal wachtwoord</b></label>
+    <input type="wachtwoord" placeholder="wachtwoord herhalen" name="psw-repeat" id="psw-repeat" required>
+    <hr>
+    <button type="register" class="registreerbtn">registreren</button>
+  </div>
+  <div class="container login">
+    <p>heb je al een account? <a href="#">inloggen</a>.</p>
+  </div>
     
 </body>
 </html>
