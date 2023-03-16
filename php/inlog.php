fef8775b9590099e50edb1d260d4ae49324e91db
karsduijn <karsduijn2004@gmail.com>
2023-03-16 09:49:00 +0100

    fix

--- php/inlog.php
+++ php/inlog.php
@@ -8,30 +8,30 @@
     <title>Document</title>
 </head>
 <body>
-  <center>
-    <form action="action_page.php" method="post">
-        <div class="imgcontainer">
-          <img src="../profile.png" alt="Avatar" class="avatar" height="10%" width="10%">
-        </div>
-      
-        <div class="container">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="uname" required>
-      
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required>
-      
-          <button type="submit">Login</button>
-          <label>
-            <input type="checkbox" checked="checked" name="remember"> Remember me
-          </label>
-        </div>
-      
-        <div class="container">
-          <button type="button" class="cancelbtn">Cancel</button>
-          <span class="psw">Forgot <a href="#">password?</a></span>
-        </div>
-      </form>
-      </center>
+    <center>
+        <form action="action_page.php" method="post">
+            <div class="imgcontainer">
+            <img src="../profile.png" alt="Avatar" class="avatar" height="10%" width="10%">
+            </div>
+        
+            <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+        
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+        
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+            </div>
+        
+            <div class="container">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
+        </form>
+    </center>
 </body>
 </html>
