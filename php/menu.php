583c6bd550a6b33b327f0138bbb7a38580463790
karsduijn <karsduijn2004@gmail.com>
2023-03-16 10:05:30 +0100

    ok

--- php/menu.php
+++ php/menu.php
@@ -1,7 +1,7 @@
 <div class="menu">
     <a href=""></a>
     <a href="computer.php">PC</a>
-    <a href="playstation">playstation</a>
+    <a href="playstation.php">playstation</a>
     <a href=""></a>
     <a href=""></a>
     <a href=""></a>
