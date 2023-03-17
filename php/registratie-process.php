865da82ff45f3c6281c1e32641f097eeffc224c6
karsduijn <karsduijn2004@gmail.com>
2023-03-17 10:15:13 +0100

    registratie fix

--- php/registratie-process.php
+++ php/registratie-process.php
@@ -12,23 +12,17 @@
 	if ($conn->connect_error) {
 	  die("Connection failed: " . $conn->connect_error);
 	}
-    
-
-    if(isset($_POST['Voornaam']) && isset($_POST['Wachtwoord'])){
-       
-
-
-		
+    if(isset($_POST['Voornaam']) && isset($_POST['Wachtwoord']))
+    {	
     }
-
-
 	$Voornaam = 	$_POST['voornaam'];
 	$Achternaam = 	$_POST['achternaam'];
 	$Email = 		$_POST['email'];
 	$Wachtwoord = 	$_POST['wachtwoord'];
 	
-	$sql = "INSERT INTO `gebruikers` (`ID`, `Voornaam`, `Achternaam`, `Email`, 'Wachtwoord') VALUES (NULL, '$Voornaam', '$Achternaam', '$Email', $Wachtwoord)";
-	$result = $conn->query($sql);
+	$sql = "INSERT INTO gebruikers (ID, Voornaam, Achternaam, Email, Wachtwoord) VALUES (NULL, '$Voornaam', '$Achternaam', '$Email', '$Wachtwoord')";
+
+    $result = $conn->query($sql);
 
     header("Location:inlog.php");
 ?>
