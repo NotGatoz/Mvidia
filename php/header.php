9ac6ada138c8caf81120e81a874c06d1c2e8bfeb
Jack Kragten <gatoz11.com@gmail.com>
2023-03-17 11:43:28 +0100

    navbar fix

--- php/header.php
+++ php/header.php
@@ -1,6 +1,6 @@
 <div class="header">
     <a href="../php/index.php" class="button">home</a> 
-    <a href="../php/registratie.php" class="button">registreren</a>
+    <a href="../php/login.php" class="button">inloggen</a>
     <a href="../php/gameupload.php" class="button">verkopen</a> 
 </div>
 <link rel=stylesheet href=../css/index.css>
